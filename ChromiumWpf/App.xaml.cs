2ab6a4e89231b4109561841cf44252047f964000
姜春辉 <gaigai3355@hotmail.com>
2019-03-21 08:43:37 +0800

    first commit

---
+++ ChromiumWpf/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChromiumWpf
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        string cef_root = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CEF");
+        public App()
+        {
+            var path = Environment.GetEnvironmentVariable("PATH");
+            Environment.SetEnvironmentVariable("PATH", path + ";" + cef_root);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var settings = new CefSettings()
+            {
+                BrowserSubprocessPath = $@"{cef_root}\CefSharp.BrowserSubprocess.exe",
+                UserDataPath = $@"{cef_root}\userdata",
+                ResourcesDirPath = cef_root,
+                LocalesDirPath = $@"{cef_root}",
+                Locale = "zh-CN",
+                CachePath = $@"{cef_root}\cache",
+            };
+            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ChromiumwpfWithoutNuget
-基于chromium的wpf应用
+基于chromium的wpf应用不依赖于NUGET
+
+CefSharp版本为71.0.2  
